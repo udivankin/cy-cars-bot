5a018a0463c9116c67999452f3513593d03bf7e2
Alexey Udivankin <alexey@udivankin.ru>
2016-07-09 11:40:21 +0300

    fix matching, add limit, add interval to host config

--- parser.js
+++ parser.js
@@ -4,17 +4,21 @@ const config = require('./config.js');
 const storage = require('./storage.js');
 
 class Parser {
-  gather() {
-    config.hosts.forEach(host => {
-      request(host.url, (err, resp, body) => {
-        if (err) {
-          console.log('Error in url request');
-          return;
-        }
+  gather(host) {
+    request(host.url, (err, resp, body) => {
+      if (err) {
+        console.log('Error in url request');
+        return;
+      }
 
-        const result = host.parser(cheerio.load(body));
-        storage.addCars(result);
-      });
+      const result = host.parser(cheerio.load(body));
+      storage.addCars(result);
+    });
+  }
+
+  gatherAll() {
+    config.hosts.forEach(host => {
+      this.gather(host);
     });
   }
 }
